581ad094b98864a676aa452212b204cb22dfbfbe
Song Khoi <songkhoi123@gmail.com>
2026-06-19 17:21:59 +0700

    fix

--- custom_addons/hr_employee_hrm_detail/migrations/19.0.1.1.82/post-migrate.py
+++ custom_addons/hr_employee_hrm_detail/migrations/19.0.1.1.82/post-migrate.py
@@ -21,17 +21,18 @@ def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     from odoo.addons.hr_employee_hrm_detail.hooks import _sync_user_visibility_policy
 
-    group_all = env.ref("hr_employee_hrm_detail.group_emp_all")
-    supporter = env.ref("hr_employee_hrm_detail.group_hr_employees_supporter")
-    if group_all in supporter.implied_ids:
+    group_all = env.ref("hr_employee_hrm_detail.group_emp_all", raise_if_not_found=False)
+    supporter = env.ref("hr_employee_hrm_detail.group_hr_employees_supporter", raise_if_not_found=False)
+    if group_all and supporter and group_all in supporter.implied_ids:
         supporter.write({"implied_ids": [(3, group_all.id)]})
 
     users = env["res.users"].search([("share", "=", False), ("id", "!=", SUPERUSER_ID)])
     _sync_user_visibility_policy(env, users)
-    for user in users:
-        login = (user.login or "").lower()
-        if login in HR_EMP_ALL_LOGINS and not user.has_group("hr.group_hr_manager"):
-            user.write({"group_ids": [(4, group_all.id)]})
+    if group_all:
+        for user in users:
+            login = (user.login or "").lower()
+            if login in HR_EMP_ALL_LOGINS and not user.has_group("hr.group_hr_manager"):
+                user.write({"group_ids": [(4, group_all.id)]})
     env.registry.clear_cache()
     _logger.info(
         "hr_employee_hrm_detail 19.0.1.1.82: removed EMP_ALL from non-admin supporters"
